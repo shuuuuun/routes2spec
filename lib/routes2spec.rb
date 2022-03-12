411588067b8a445220d0ada3c7daf6a914fc5d75
shuuuuuny <shuuuuuny+git@gmail.com>
2022-03-12 13:40:57 +0900

    init gem
    $ bundle gem routes2spec --test=rspec --mit

---
+++ lib/routes2spec.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "routes2spec/version"
+
+module Routes2spec
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/routes2spec/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Routes2spec
+  VERSION = "0.1.0"
+end
